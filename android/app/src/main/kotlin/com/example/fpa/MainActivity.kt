c5e85cc68c2a764503dfbfb93c101d1e3eeb1da9
yuri.nascimento <yuri.nascimento@pizzahut.com>
2021-03-28 15:19:19 -0300

    Reorganizando estrutura de pasta

---
+++ android/app/src/main/kotlin/com/example/fpa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fpa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- fpa/android/app/src/main/kotlin/com/example/fpa/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fpa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
